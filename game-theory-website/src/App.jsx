d2955e623e9976c87bd71ae880d4a05107db6963
Kiersten E <152218847+kiengen@users.noreply.github.com>
2025-03-14 02:17:10 -0400

    old website and beginnings of new

---
+++ game-theory-website/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex-v">
+      <text className="big-font">Darwin Game</text>
+      <text className="c button bg-b bordered">Get Started</text>
+    </div>
+  )
+}
+
+export default App
